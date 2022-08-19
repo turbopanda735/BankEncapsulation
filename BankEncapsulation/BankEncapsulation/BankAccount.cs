eeb148dc226ef5b36ed07da8d71c1a3834be6038
Joel <franco4prez@gmail.com>
2022-08-19 12:12:21 -0400

    first commit

---
+++ BankEncapsulation/BankEncapsulation/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+        private readonly int _pin = 1234;
+        private bool _locked = true;
+        public bool EnterPIN(int pin)
+        {
+            if (_pin == pin)
+            {
+                _locked = false;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Deposit(double amount)
+        {
+            if (_locked != false)
+                Console.WriteLine("sorry you cannot access this account");
+            else _balance += amount;
+        }
+        public void Withdrawl(double amount)
+        {
+            if (_locked != false)
+                Console.WriteLine("sorry you cannot access this account");
+            else _balance -= amount;
+        }
+        public string GetBalance()
+        {
+            if (_locked != false)
+                return "sorry you cannot access this account";
+            else return _balance.ToString();
+        }
+    }
+}
+
---
+++ BankEncapsulation/BankEncapsulation/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main()
+        {
+            var myAccount = new BankAccount();
+            Console.WriteLine("Welcome to The Bank");
+            bool correctPIN;
+            do
+            {
+                Console.WriteLine("please enter your pin number");
+                var pinAttempt = int.Parse(Console.ReadLine());
+                correctPIN = myAccount.EnterPIN(pinAttempt);
+            } while (!correctPIN);
+
+            var finishedAction = false;
+            do
+            {
+                var finishedActionATM = false;
+                Console.WriteLine("please select the action you would like to perform");
+                Console.WriteLine("press 1 to withdrawl, press 2 to deposit, press 3 to access account balance, and press 4 to exit");
+                var accountAction = int.Parse(Console.ReadLine());
+
+                switch (accountAction)
+                {
+                    case 1:
+                        Console.WriteLine("please enter the amount youd like to withdraw");
+                        var withdrawl = int.Parse(Console.ReadLine());
+                        myAccount.Withdrawl(withdrawl);
+                        break;
+                    case 2:
+                        Console.WriteLine("please enter the amount youd like to deposit");
+                        var deposit = int.Parse(Console.ReadLine());
+                        myAccount.Deposit(deposit);
+                        break;
+                    case 3:
+                        Console.WriteLine($"your current balance is: {myAccount.GetBalance()}");
+                        break;
+                    case 4:
+                        finishedActionATM = true;
+                        break;
+                }
+                if (finishedActionATM == true)
+                    finishedAction = true;
+            } while (!finishedAction);
+        }
+    }
+}
